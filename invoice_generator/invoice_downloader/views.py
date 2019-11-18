7f69c40cce311302b098538efeb5a6927052a0a5
Francisca <franciscacruzat23@gmail.com>
2019-11-18 15:49:06 -0300

    Archivo se agrega y se puede descargar

--- invoice_generator/invoice_downloader/views.py
+++ invoice_generator/invoice_downloader/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.template.loader import select_template
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
@@ -15,11 +16,25 @@ def index(request):
         return HttpResponse(template.render(context, request))
 
 
-def test2(request):
+def nueva(request):
     
     if request.method == 'GET':
-        template = select_template(['test2.html'])
+        template = select_template(['nueva.html'])
+       
         context = {
         }
+    
+    if request.method == 'POST' and request.FILES['document']:
+        document = request.FILES['document']
+        fs = FileSystemStorage()
+        filename = fs.save(document.name, document)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'nueva.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+        # print(uploaded_file.name)
+        # print(uploaded_file.size)
 
-        return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(context, request))
+
+        
--- invoice_generator/invoice_generator/settings.py
+++ invoice_generator/invoice_generator/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
@@ -118,10 +119,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'mysite/static')
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
+
+# VER PARA QUÈ ES ESO
+
 
 
 # LOGIN REDIRECTS
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'login'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
